150ff041bfc3f4f5d8ae39f45e6aeb2c5ddf6203
Saurav Subedi <subedisaurav331@gmail.com>
2025-07-24 23:09:46 +0900

    OPtimized js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ fetch("Kanji.json")
     console.error("Failed to load kanji data", err);
   });
 
-//flashcard logic
+//flashcard
 function showcard(index) {
   const card = flashcards[index];
   KanjiDiv.textContent = card.kanji;
